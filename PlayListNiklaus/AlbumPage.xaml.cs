559da7a7da2e697d7d51616c3e37257a19b57879
MrGrinser <niklaus.koch@bluewin.ch>
2024-06-16 00:01:37 +0200

    newpage

---
+++ PlayListNiklaus/AlbumPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace PlayListNiklaus
+{
+    public partial class AlbumPage : ContentPage
+    {
+        public AlbumPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- PlayListNiklaus/MainPageViewModel.cs
+++ PlayListNiklaus/MainPageViewModel.cs
@@ -66,6 +66,7 @@ namespace PlayListNiklaus
         }
 
         public ICommand SearchCommand { get; }
+        public ICommand AlbumSelectedCommand { get; }
 
         public MainPageViewModel()
         {
@@ -73,23 +74,43 @@ namespace PlayListNiklaus
             SearchCommand = new Command(FilterAlbums);
             IsDarkMode = false;
             UpdateBackgroundColor();
+            AlbumSelectedCommand = new Command<Album>(OnAlbumSelected);
+        }
+
+        private Album _selectedAlbum;
+        public Album SelectedAlbum
+        {
+            get { return _selectedAlbum; }
+            set
+            {
+                _selectedAlbum = value;
+                OnPropertyChanged();
+                if (_selectedAlbum != null)
+                {
+                    OnAlbumSelected(_selectedAlbum);
+                }
+            }
+        }
+
+        private void OnAlbumSelected(Album selectedAlbum)
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new AlbumPage { BindingContext = new AlbumPageViewModel(selectedAlbum) });
         }
 
         private void LoadMockAlbums()
         {
             Albums = new ObservableCollection<Album>
             {
-            new Album { Title = "Ace of Spades", Artist = "Motörhead", CoverImage = "heavymetall.png" },
-            new Album { Title = "Classical Masterpieces", Artist = "Beethoven", CoverImage = "beet.png" },
-            new Album { Title = "News of the World", Artist = "Queen", CoverImage = "green.png" },
-            new Album { Title = "Infinite", Artist = "Eminem", CoverImage = "hiphop1.png" },
-            new Album { Title = "Encore", Artist = "Eminem", CoverImage = "hiphop2.png" },
-            new Album { Title = "Louis Under the Stars", Artist = "Louis Armstrong", CoverImage = "jazz.png" },
-            new Album { Title = "Recovery", Artist = "Eminem", CoverImage = "hiphop3.png" },
-            new Album { Title = "A Kind of Magic", Artist = "Queen", CoverImage = "rock.png" },
-            new Album { Title = "Beethoven Piano Music", Artist = "Beethoven", CoverImage = "piano.png" },
-            new Album { Title = "The Game", Artist = "Queen", CoverImage = "rock2.png" }
-
+                new Album { Title = "Ace of Spades", Artist = "Motörhead", CoverImage = "heavymetall.png" },
+                new Album { Title = "Classical Masterpieces", Artist = "Beethoven", CoverImage = "beet.png" },
+                new Album { Title = "News of the World", Artist = "Queen", CoverImage = "green.png" },
+                new Album { Title = "Infinite", Artist = "Eminem", CoverImage = "hiphop1.png" },
+                new Album { Title = "Encore", Artist = "Eminem", CoverImage = "hiphop2.png" },
+                new Album { Title = "Louis Under the Stars", Artist = "Louis Armstrong", CoverImage = "jazz.png" },
+                new Album { Title = "Recovery", Artist = "Eminem", CoverImage = "hiphop3.png" },
+                new Album { Title = "A Kind of Magic", Artist = "Queen", CoverImage = "rock.png" },
+                new Album { Title = "Beethoven Piano Music", Artist = "Beethoven", CoverImage = "piano.png" },
+                new Album { Title = "The Game", Artist = "Queen", CoverImage = "rock2.png" }
             };
 
             FilteredAlbums = new ObservableCollection<Album>(Albums);
@@ -117,10 +138,35 @@ namespace PlayListNiklaus
         }
     }
 
-    public class Album
+    public class Album : BindableObject
     {
+        private bool _isSelected;
+
         public string Title { get; set; }
         public string Artist { get; set; }
         public string CoverImage { get; set; }
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+
+    public class AlbumPageViewModel : BindableObject
+    {
+        public Album SelectedAlbum { get; }
+
+        public AlbumPageViewModel(Album selectedAlbum)
+        {
+            SelectedAlbum = selectedAlbum;
+        }
     }
 }
