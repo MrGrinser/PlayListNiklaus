63c8987a478d06aab7a7ceac081d81b07c8f944c
MrGrinser <niklaus.koch@bluewin.ch>
2024-06-16 07:11:38 +0200

    aer

--- PlayListNiklaus/PlaylistPageViewModel.cs
+++ PlayListNiklaus/PlaylistPageViewModel.cs
@@ -3,18 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PlayListNiklaus
 {
     public class PlaylistPageViewModel : BindableObject
     {
         public Playlist SelectedPlaylist { get; set; }
-        public Color BackgroundColor { get; set; }
+        public Color Background { get; set; }
+        private Song _selectedSong;
+        private bool _isSongSelected;
+
 
         public PlaylistPageViewModel(Playlist selectedPlaylist, Color backgroundColor)
         {
             SelectedPlaylist = selectedPlaylist;
-            BackgroundColor = backgroundColor;
+            Background = backgroundColor;
+            SongSelectedCommand = new Command<Song>(OnSongSelected);
+        }
+        public Song SelectedSong
+        {
+            get => _selectedSong;
+            set
+            {
+                _selectedSong = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsSongSelected
+        {
+            get => _isSongSelected;
+            set
+            {
+                _isSongSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SongSelectedCommand { get; }
+
+        private void OnSongSelected(Song song)
+        {
+            SelectedSong = song;
+            IsSongSelected = true;
+
+            // Update the IsSelected property for each song
+            foreach (var s in SelectedPlaylist.Songs)
+            {
+                s.IsSelected = s == song;
+            }
         }
 
         // Other properties and methods of PlaylistPageViewModel
